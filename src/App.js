9624b2fd7d7ab15004a008c35f1af7c647c6c157
Leigh Halliday <leighhalliday@gmail.com>
2018-12-09 17:01:12 -0500

    Putting together ref demo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createRef, useRef } from "react";
 
-class App extends Component {
+class CallbackRefExample extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          alert(this.inputRef.value);
+        }}
+      >
+        <input
+          type="text"
+          defaultValue="callback"
+          ref={input => {
+            this.inputRef = input;
+          }}
+        />
+        <button>Submit</button>
+      </form>
     );
   }
 }
 
-export default App;
+class CreateRefExample extends React.Component {
+  inputRef = createRef();
+
+  render() {
+    return (
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          alert(this.inputRef.current.value);
+        }}
+      >
+        <input ref={this.inputRef} type="text" defaultValue="createRef" />
+        <button>Submit</button>
+      </form>
+    );
+  }
+}
+
+function UseRefExample() {
+  const inputRef = useRef();
+
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        alert(inputRef.current.value);
+      }}
+    >
+      <input ref={inputRef} type="text" defaultValue="useRef" />
+      <button>Submit</button>
+    </form>
+  );
+}
+
+export default function App() {
+  return (
+    <div>
+      <CallbackRefExample />
+      <CreateRefExample />
+      <UseRefExample />
+    </div>
+  );
+}
